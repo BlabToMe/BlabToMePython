23d0893b8b160d97df86ee9230f735cdf70bebd6
Mathew <mathew@mathewjenkinson.co.uk>
2016-02-19 16:15:25 -0500

    Update BlabToMe.py
    code corrections

--- BlabToMe.py
+++ BlabToMe.py
@@ -1,28 +1,27 @@
 #!/usr/bin/env python3
-
 import requests
 
 def SendBlabToEndPoints(BlabToMeUsername, BlabToMePassword, BlabMessage, EndPointCSV):
-  POSTPayLoad = {'userName': BlabToMeUsername, 'Password': BlabToMePassword, 'Message':'BlabMessage', 'SendTo':'EndPointCSV'}
+  POSTPayLoad = {'UserName': BlabToMeUsername, 'Password': BlabToMePassword, 'Message':BlabMessage, 'SendTo':EndPointCSV}
   r = requests.post("https://api.blabto.me/Inbound/Inbound", auth=(BlabToMeUsername, BlabToMePassword), data=POSTPayLoad)
-  if r.status_code == 404:
+  if r.status_code == 401:
     return '{"Success":false,"Response":"Username and Password are not correct"}'
   else:
     return r.json()
 
-def GetBlabsForUser(BlabToMeUsername, BlabToMePassword)
-  POSTPayLoad = {'userName': BlabToMeUsername, 'Password': BlabToMePassword}
+def GetBlabsForUser(BlabToMeUsername, BlabToMePassword):
+  POSTPayLoad = {'UserName': BlabToMeUsername, 'Password': BlabToMePassword}
   r = requests.post("https://api.blabto.me/API/GetBlabsForUser", auth=(BlabToMeUsername, BlabToMePassword), data=POSTPayLoad)
-  if r.status_code = 404:
-    return "{"Success":false,"Response":"Username and Password are not correct"}"
+  if r.status_code == 401:
+    return '{"Success":false,"Response":"Username and Password are not correct"}'
   else:
     return r.json()
   
-def GetBlabsForEndPoint(BlabToMeUsername, BlabToMePassword, EndPointToQuery)
-  POSTPayLoad = {'userName': BlabToMeUsername, 'Password': BlabToMePassword, 'Endpoint':EndPointToQuery}
+def GetBlabsForEndPoint(BlabToMeUsername, BlabToMePassword, EndPointToQuery):
+  POSTPayLoad = {'UserName': BlabToMeUsername, 'Password': BlabToMePassword, 'Endpoint':EndPointToQuery}
   r = requests.post("https://api.blabto.me/API/GetBlabsForUser", auth=(BlabToMeUsername, BlabToMePassword), data=POSTPayLoad)
-  if r.status_code = 404:
-    return "{"Success":false,"Response":"Username and Password are not correct"}"
+  if r.status_code == 401:
+    return '{"Success":false,"Response":"Username and Password are not correct"}'
   else:
     return r.json()
   
