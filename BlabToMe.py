b1066a0893182a83829f632eca9f69c1d5fac198
Mathew <mathew@mathewjenkinson.co.uk>
2016-02-19 09:28:14 -0500

    Added the core functions of the helper library
    Added the three core functions of the helper library;
    Ability to send a Blab to known endpoints
    Ability to get Blabs for a user
    Ability to get Blabs for a specific endpoint

--- BlabToMe.py
+++ BlabToMe.py
@@ -3,6 +3,26 @@
 import requests
 
 def SendBlabToEndPoints(BlabToMeUsername, BlabToMePassword, BlabMessage, EndPointCSV)
-POSTPayLoad = {'userName': BlabToMeUsername, 'Password': BlabToMePassword, 'Message':'BlabMessage', 'SendTo':'EndPointCSV'}
-r = requests.post("http://httpbin.org/post", auth=(username, BlabToMePassword), data=POSTPayLoad)
-r.json()
+  POSTPayLoad = {'userName': BlabToMeUsername, 'Password': BlabToMePassword, 'Message':'BlabMessage', 'SendTo':'EndPointCSV'}
+  r = requests.post("https://api.blabto.me/Inbound/Inbound", auth=(username, BlabToMePassword), data=POSTPayLoad)
+  if r.status_code = 404:
+    return "{"Success":false,"Response":"Username and Password are not correct"}"
+  else:
+    return r.json()
+
+def GetBlabsForUser(BlabToMeUsername, BlabToMePassword)
+  POSTPayLoad = {'userName': BlabToMeUsername, 'Password': BlabToMePassword}
+  r = requests.post("https://api.blabto.me/API/GetBlabsForUser", auth=(username, BlabToMePassword), data=POSTPayLoad)
+  if r.status_code = 404:
+    return "{"Success":false,"Response":"Username and Password are not correct"}"
+  else:
+    return r.json()
+  
+def GetBlabsForEndPoint(BlabToMeUsername, BlabToMePassword, EndPointToQuery)
+  POSTPayLoad = {'userName': BlabToMeUsername, 'Password': BlabToMePassword, 'Endpoint':EndPointToQuery}
+  r = requests.post("https://api.blabto.me/API/GetBlabsForUser", auth=(username, BlabToMePassword), data=POSTPayLoad)
+  if r.status_code = 404:
+    return "{"Success":false,"Response":"Username and Password are not correct"}"
+  else:
+    return r.json()
+  
