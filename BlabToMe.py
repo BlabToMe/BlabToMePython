44824c31121aa1ab28d7a8efdd4689155c0e51c0
Mathew <mathew@mathewjenkinson.co.uk>
2016-02-19 15:07:09 -0500

    Update BlabToMe.py
    code corrections

--- BlabToMe.py
+++ BlabToMe.py
@@ -2,17 +2,17 @@
 
 import requests
 
-def SendBlabToEndPoints(BlabToMeUsername, BlabToMePassword, BlabMessage, EndPointCSV)
+def SendBlabToEndPoints(BlabToMeUsername, BlabToMePassword, BlabMessage, EndPointCSV):
   POSTPayLoad = {'userName': BlabToMeUsername, 'Password': BlabToMePassword, 'Message':'BlabMessage', 'SendTo':'EndPointCSV'}
-  r = requests.post("https://api.blabto.me/Inbound/Inbound", auth=(username, BlabToMePassword), data=POSTPayLoad)
-  if r.status_code = 404:
-    return "{"Success":false,"Response":"Username and Password are not correct"}"
+  r = requests.post("https://api.blabto.me/Inbound/Inbound", auth=(BlabToMeUsername, BlabToMePassword), data=POSTPayLoad)
+  if r.status_code == 404:
+    return '{"Success":false,"Response":"Username and Password are not correct"}'
   else:
     return r.json()
 
 def GetBlabsForUser(BlabToMeUsername, BlabToMePassword)
   POSTPayLoad = {'userName': BlabToMeUsername, 'Password': BlabToMePassword}
-  r = requests.post("https://api.blabto.me/API/GetBlabsForUser", auth=(username, BlabToMePassword), data=POSTPayLoad)
+  r = requests.post("https://api.blabto.me/API/GetBlabsForUser", auth=(BlabToMeUsername, BlabToMePassword), data=POSTPayLoad)
   if r.status_code = 404:
     return "{"Success":false,"Response":"Username and Password are not correct"}"
   else:
@@ -20,7 +20,7 @@ def GetBlabsForUser(BlabToMeUsername, BlabToMePassword)
   
 def GetBlabsForEndPoint(BlabToMeUsername, BlabToMePassword, EndPointToQuery)
   POSTPayLoad = {'userName': BlabToMeUsername, 'Password': BlabToMePassword, 'Endpoint':EndPointToQuery}
-  r = requests.post("https://api.blabto.me/API/GetBlabsForUser", auth=(username, BlabToMePassword), data=POSTPayLoad)
+  r = requests.post("https://api.blabto.me/API/GetBlabsForUser", auth=(BlabToMeUsername, BlabToMePassword), data=POSTPayLoad)
   if r.status_code = 404:
     return "{"Success":false,"Response":"Username and Password are not correct"}"
   else:
